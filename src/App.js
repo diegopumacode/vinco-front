fb615a6878f26e25baf17bd625e831fd2fc3ed34
diegopumacode <diegopumacode@gmail.com>
2021-11-22 11:36:43 -0500

    adding card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
-import { Container } from '@chakra-ui/layout';
+import { Container, SimpleGrid } from '@chakra-ui/layout';
+import Card from './components/Card';
+import { Button } from '@chakra-ui/button';
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { useDisclosure } from '@chakra-ui/hooks';
+
+
+const users = [
+  { "id": 1, "lastName": "Pumaa", "firstName": "awitaaaaa", "occupation": "developerrrr", "age": 22, "status": 1 }
+]
 
 function App() {
+
   return (
     <Container maxWidth='container.lg' paddingY={10}>
-      asdsad
+      <SimpleGrid childWidth={['100%', '100%', '200px']} columns={4} spacing="30px" marginBottom={12}>
+        {users.map(user => {
+          return (<>
+            <Card name={`${user.firstName} ${user.lastName}`} occupation={user.occupation}>
+              <Button colorScheme="red" variant="solid" color='white' size="sm">
+                <AiFillDelete />
+              </Button>
+            </Card>
+
+
+
+          </>)
+        })}
+
+      </SimpleGrid>
     </Container>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button } from '@chakra-ui/button'
+import { Image } from '@chakra-ui/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+
+export default function Card({name,occupation,children}) {
+    return (
+        <Box  minHeight='180px' position='relative' boxShadow="md" rounded="md" padding={5}>
+            <Flex flexDirection='column' justifyContent='space-between' height='200px'>
+                <Box>
+                    <Text fontWeight='bold'
+                        fontSize='1.3rem'
+                        marginBottom='5px'>
+                        {name}
+                    </Text>
+
+                    <Box background='green.200'
+                        fontSize='.8rem'
+                        display='inline'
+                        borderRadius='30px'
+                        paddingX={4}
+                        paddingY={1}>
+                        {occupation}
+                    </Box>
+                </Box>
+                <Flex gridGap={2}>
+                    {children}
+                </Flex>
+            </Flex>
+
+
+            <Image position='absolute' zIndex='-1' src='https://cdn-icons-png.flaticon.com/512/4815/4815145.png' width='140px' bottom='0' right='0' opacity='.6' />
+
+
+
+
+        </Box>
+    )
+}
